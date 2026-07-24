65ae30324e59c7571d8dc64a2d5af5cbeadf948d
unknown <dev.khalid.0@gmail.com>
2026-07-24 12:02:08 +0300

    improve the payment info design

--- app/(main)/test-takers/booking/payment/page.tsx
+++ app/(main)/test-takers/booking/payment/page.tsx
@@ -6,8 +6,8 @@ export default async function Page() {
   const translations = await getTranslations();
   return (
     <div id="midd-wrapper">
-      <section className="cmn-section lightgrey-bg screen20-first-section">
-        <div className="container">
+      <section className="cmn-section lightgrey-bg screen20-first-section w-full">
+        <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
           <PaymentWrapper translations={translations} />
         </div>
       </section>
